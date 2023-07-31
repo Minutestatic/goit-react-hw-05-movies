03f8a1d840ee4ec0b37ff115c2df6f2736da18b3
Maksym Lahutin <lagutinmaksim.98@gmail.com>
2023-07-31 04:32:49 +0300

    Update Cast.jsx

--- src/components/Cast/Cast.jsx
+++ src/components/Cast/Cast.jsx
@@ -7,12 +7,10 @@ const { useParams } = require('react-router-dom');
 const Cast = () => {
   const { movieId } = useParams();
   const [cast, setCast] = useState([]);
-  console.log(cast);
 
   useEffect(() => {
     fetchCredits(movieId)
       .then(result => {
-        console.log(result);
         setCast(result);
       })
       .catch(error => {
